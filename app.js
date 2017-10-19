1f02b5940d496586f15c6643e3b34aa38e808da6
Bruno Silvano Costa <bruno_705@hotmail.com>
2017-10-18 23:43:38 -0200

    Mongoose connection setup

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+// Mongoose ODM
+var mongoose = require('mongoose');
+
+// Connect to Mongoose
+mongoose.connect('mongodb://localhost:27017/standupmeetingnotes', {
+  useMongoClient: true
+});
+
 var app = express();
 
 // assign the swig view engine to .html files
